44f26466c9a52baca7802988abb1d940781e1d17
Zzzz-yx <150459223+Zzzz-yx@users.noreply.github.com>
2024-02-22 21:24:31 +0800

    Add files via upload

--- 阶段七考核/tcp.go
+++ 阶段七考核/tcp.go
@@ -1,39 +1,39 @@
 package main
 
-import {
-	"net"
-	"log"
+import (
 	"fmt"
-}
+	"io"
+	"log"
+	"net"
+	"time"
+)
 
-func main(){
-	listener,err := net.listen( network: "tcp", address: "127.0.0.1:8000")//监听tcp协议的端口
+func main() {
+	listener, err := net.Listen("tcp", "127.0.0.1:8000") //监听tcp协议的端口
 	if err != nil {
-        log.Fatal(err) //如果错误会退出
+		log.Fatal(err) //如果错误会退出
 	}
 
 	for {
 		//会阻塞，接收客户端的连接
 		fmt.Println("accept before")
 		conn, err := listener.Accept()
-		fmt.Println("accept after",conn.RemoteAddr().String())//获取远程地址
+		fmt.Println("accept after", conn.RemoteAddr().String()) //获取远程地址
 		if err != nil {
 			log.Print(err)
 			continue
 		}
-		
+
 		handleConn(conn)
 	}
 }
 
-
-
-func handleConn(conn net.Conn){
+func handleConn(conn net.Conn) {
 	defer conn.Close()
 	//发送当前时间给客户端
-	for{
-		err := io.WriteString(conn,time.Now().Format(layout:"2006-01-02 15:04:05"))//格式化时间
-		if err != nil{
+	for {
+		_, err := io.WriteString(conn, time.Now().Format("2006-01-02 15:04:05")) //格式化时间
+		if err != nil {
 			break
 		}
 		time.Sleep(1 * time.Second)
